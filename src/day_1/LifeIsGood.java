0b3d66404b4de86276b0bcc18a1772b14270b428
Alex-Starik <oleksandrstarik@gmail.com>
2020-04-24 15:02:22 -0400

    LifeIsGood.js

--- src/day_1/LifeIsGood.java
+++ src/day_1/LifeIsGood.java
@@ -1,7 +1,10 @@
 package day_1;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 public class LifeIsGood {
     public static void main(String[] args) {
         System.out.println("I love my Life!");
+        System.out.println("I love my family!");
     }
 }
