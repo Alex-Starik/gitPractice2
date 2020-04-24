fb4632bc9f9fe79daf6d2a85a461c0ec2673be4c
Alex-Starik <64120644+Alex-Starik@users.noreply.github.com>
2020-04-24 16:30:02 -0400

    Update LifeIsGood.java

--- src/day_1/LifeIsGood.java
+++ src/day_1/LifeIsGood.java
@@ -6,5 +6,7 @@ public class LifeIsGood {
     public static void main(String[] args) {
         System.out.println("I love my Life!");
         System.out.println("I love my family!");
+        
+        System.out.println("I love this world!") // this line was added through GitHub
     }
 }
