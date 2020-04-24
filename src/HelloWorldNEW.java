3a271467374c4e11b3b58b9d1a9e458c978c565d
Alex-Starik <oleksandrstarik@gmail.com>
2020-04-24 14:57:36 -0400

    LifeIsGood.js

---
+++ src/HelloWorldNEW.java
@@ -0,0 +1,5 @@
+public class HelloWorldNEW {
+    public static void main(String[] args) {
+        System.out.println("Hello NEW World");
+    }
+}
---
+++ src/day_1/LifeIsGood.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class LifeIsGood {
+    public static void main(String[] args) {
+        System.out.println("I love my Life!");
+    }
+}
