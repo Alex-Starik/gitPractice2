70ef541eac8062039f3c5664c60aba7de5caf26c
Alex-Starik <64120644+Alex-Starik@users.noreply.github.com>
2020-04-28 16:00:00 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -3,5 +3,7 @@ package day_1;
 public class HappyBirthday {
   public static void main (String [] args) {
     System.out.println("This message was added from GitHub");
+    
+    System.out.println("This one as well");
   }
 }
